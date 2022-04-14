0b529050f6d9523a00b67d82170b5ac804a4cfd4
Jakup Lutzen <jakup@lutzen.co>
2022-04-14 23:44:40 +0200

    added neovim config

--- .xmonad/battery/batterynotification.sh
+++ .xmonad/battery/batterynotification.sh
@@ -5,84 +5,110 @@ marginthree=10
 marginfour=5
 oldbatt=1
 oldcharg="unset"
-getstatus() {
-    status=$(acpi -b)
-}
-getcharg() {
-    charging=$(echo $status | awk '{print $3}')
-}
-getbatt() {
-    battery_level=$(echo $status | grep -P -o '[0-9]+(?=%)') #define command getbatt. should store percentage as int
-}
-alarmmess() {
-    xmessage -buttons "Okay" -title "batteryalert" "ALERT! Battery is at $battery_level%" &
-    aplay /home/jakupl/.xmonad/battery/batterysound.wav &
-}
-batteryon() {
-    aplay /home/jakupl/.xmonad/battery/batteryon.wav
-}
-batteryoff() {
-    aplay /home/jakupl/.xmonad/battery/batteryoff.wav
-}
-batteryfull() {
-    aplay /home/jakupl/.xmonad/battery/batteryfull.wav
-}
-sleep 10
-getstatus
-getbatt
 
-if [ $battery_level -lt $((marginone+1)) ]
-then
-    alarmmess
-fi
-while true
-do
+
+main() {
+    sleep 10
     getstatus
-    getcharg
     getbatt
-    if [ "$oldcharg" == "Discharging," ] && [ "$charging" == "Charging," ]
-    then
-        batteryon
-    elif [ "$oldcharg" == "Charging," ] && [ "$charging" == "Discharging," ]
-    then
-        batteryoff
-	if [ $battery_level -lt $((marginone+1)) ]
-	then
-	   sleep 3
-	   alarmmess
-	fi 
-    elif [ "$oldcharg" == "Full," ] && [ "$charging" == "Discharging," ]
-    then
-	batteryoff
-	if [ $battery_level -lt $((marginone+1)) ]
-	then
-	   sleep 3
-	   alarmmess
-	fi
-    elif [ "$oldcharg" == "Discharging," ] && [ "$charging" == "Full," ]
-    then
-	batteryfull
-    fi
-    if [ "$charging" == "Discharging," ] || [ "$charging" == "Charging," ] || [ "$charging" == "Full," ]
-    then
-	oldcharg=$charging
-    fi
-    if [ $battery_level -lt $((marginone+1)) ] && [ $oldbatt -gt $marginone ]
+    
+    if [ $battery_level -lt $((marginone+1)) ]
     then
         alarmmess
     fi
-    if [ $battery_level -lt $((margintwo+1)) ] && [ $oldbatt -gt $margintwo ]
-    then
-	alarmmess
-    fi
-    if [ $battery_level -lt $((marginthree+1)) ] && [ $oldbatt -gt $marginthree ]
-    then
-	alarmmess
-    fi
-    if [ $battery_level -lt $((marginfour+1)) ] && [ $oldbatt -gt $marginfour ]
-    then
-	alarmmess
-    fi
-    oldbatt="$battery_level"
-    sleep 5
-done
+    while true
+    do
+        getstatus
+        getcharg
+        getbatt
+        if [ "$oldcharg" == "Discharging," ] && [ "$charging" == "Charging," ]
+        then
+            batteryon
+        elif [ "$oldcharg" == "Charging," ] && [ "$charging" == "Discharging," ]
+        then
+            batteryoff
+    	if [ $battery_level -lt $((marginone+1)) ]
+    	then
+    	   sleep 3
+    	   alarmmess
+    	fi 
+        elif [ "$oldcharg" == "Full," ] && [ "$charging" == "Discharging," ]
+        then
+    	batteryoff
+    	if [ $battery_level -lt $((marginone+1)) ]
+    	then
+    	   sleep 3
+    	   alarmmess
+    	fi
+        elif [ "$oldcharg" == "Discharging," ] && [ "$charging" == "Full," ]
+        then
+    	batteryfull
+        fi
+        if [ "$charging" == "Discharging," ] || [ "$charging" == "Charging," ] || [ "$charging" == "Full," ]
+        then
+    	oldcharg=$charging
+        fi
+        if [ $battery_level -lt $((marginone+1)) ] && [ $oldbatt -gt $marginone ]
+        then
+            alarmmess
+        fi
+        if [ $battery_level -lt $((margintwo+1)) ] && [ $oldbatt -gt $margintwo ]
+        then
+    	alarmmess
+        fi
+        if [ $battery_level -lt $((marginthree+1)) ] && [ $oldbatt -gt $marginthree ]
+        then
+    	alarmmess
+        fi
+        if [ $battery_level -lt $((marginfour+1)) ] && [ $oldbatt -gt $marginfour ]
+        then
+    	alarmmess
+        fi
+        oldbatt="$battery_level"
+        sleep 5
+    done
+}
+
+
+getstatus() {
+    status=$(acpi -b)
+}
+
+
+getcharg() {
+    charging=$(echo $status | awk '{print $3}')
+}
+
+
+getbatt() {
+    battery_level=$(echo $status | grep -P -o '[0-9]+(?=%)') #define command getbatt. should store percentage as int
+}
+
+
+alarmmess() {
+#    xmessage -buttons "Okay" -title "batteryalert" "ALERT! Battery is at $battery_level%" & # Going with dunstify atm
+    dunstify -a "chargestatus" -u critical -h string:x-dunst-stac-tag:tag "ALERT! Battery is at $battery_level%" &
+    aplay /home/jakupl/.xmonad/battery/batterysound.wav &
+}
+
+
+batteryon() {
+    aplay /home/jakupl/.xmonad/battery/batteryon.wav &
+    dunstify -a "chargestatus" -u normal -h string:x-dunst-stac-tag:tag "Charging"
+}
+
+
+batteryoff() {
+    aplay /home/jakupl/.xmonad/battery/batteryoff.wav &
+    dunstify -a "chargestatus" -u normal -h string:x-dunst-stac-tag:tag "Discharging"
+}
+
+
+batteryfull() {
+    aplay /home/jakupl/.xmonad/battery/batteryfull.wav &
+    dunstify -a "chargestatus" -u normal -h string:x-dunst-stac-tag:tag "Charging (full)"
+}
+
+
+# Running main!
+main
